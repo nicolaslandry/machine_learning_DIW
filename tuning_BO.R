1ecc6dea7f1df9c3affcbdc6cf97f7fcf850420f
nicolaslandry <nicolas.landry@mines-paristech.fr>
2017-01-26 11:18:08 +0100

    Update tuning_BO.R
    tunegrid different from v02 (cp and tsetsize change)the code is not split anymore. 
    we removed combinations that were already computed with v02
    the sampling step is slightly different, since we make sure that there is at least 1 GCR in ids.train before upsampling
    only 50 trees max, and ntree =seq(10,50,10), instead of 100 trees max and ntree = seq(20,100,20)
    Each combination is saved in a different file. "boosting_output_2_***.RData"
    ##### this code did not work, because of several coding mistakes #####

--- tuning_BO.R
+++ tuning_BO.R
@@ -1,12 +1,12 @@
 ## TUNING BOOSTING
 ## NICOLAS LANDRY  created the 15/12/2016
-## LAST MODIFICATION: 15/12/2016
+## LAST MODIFICATION: 11/01/2017
 ##OBJECTIVE: How to improve result from bo?
 
 
 rm(list=ls())
 setwd("H:\\BOOSTING\\")
-SPLIT=1
+
 #=====================================================
 #================= 1 LIBRARIES LOAD ==================
 #=====================================================
@@ -51,8 +51,8 @@ alldata=alldata[-ids.japan,]
 
 colnames=c("type","t11","t12","t13","t14","t15","t16","t17","t21","t31","t32","t33","t35","t41","t42","t43")
 cnames=which(is.element(colnames(alldata),colnames))
-ids=which(alldata$type=="LWR"|alldata$type=="GCR")
-
+countries=c("FRANCE","GERMANY","JAPAN","SPAIN","SWITZERLAND","UNITED KINGDOM","UNITED STATES OF AMERICA")
+ids=which((alldata$type=="LWR"|alldata$type=="GCR") & alldata$country %in% countries)
 d=alldata[ids,c(cnames)]
 d$type=factor(d$type)
 d=na.omit(d)
@@ -63,20 +63,17 @@ for(i in 1:nrow(d)){
   S[i]=(sum(d[i,2:(ncol(d)-1)]))
 }
 d<-d[-which(S==0),]
-rm(S)
-rm(i)
-rm(ids)
-rm(cnames)
-rm(ids.japan)
+rm(S,i,ids,cnames,ids.japan,countries)
+
 #=====================================================
 #================== 5 ANALYSIS =======================
 #=====================================================
 
-#PARALLEL PROCESSING		
-max_cores <- detectCores()[1]		
-max_cores		
-if (max_cores > 25) max_cores <- 25		
-cl <- makeCluster(max_cores, homogeneous=T)		
+#PARALLEL PROCESSING
+max_cores <- detectCores()[1]
+max_cores
+if (max_cores > 25) max_cores <- 7
+cl <- makeCluster(max_cores, homogeneous=T)
 registerDoParallel(cl)
 
 seed=7
@@ -85,20 +82,34 @@ set.seed(seed)
 #### WITH FOREACH LOOP
 tunegrid <- expand.grid(.minsplit=seq(3,100,20),
                         .minbucket=seq(1,20,5),
-                        .cp=c(0.001,0.01,0.1),
-                        .tsetsize=seq(100,1100,100),
+                        .cp=c(0.1,0.2,0.3),
+                        .tsetsize=seq(300,1100,100),
                         .ratio=0.5,
                         .coeflearn=c("Breiman","Freund"))
-j <- nrow(tunegrid)
+k <- nrow(tunegrid)
 
-tunegrid <- tunegrid[((SPLIT-1)*j/6+1):(SPLIT*j/6),]
 
-k=nrow(tunegrid)
+#### REMOVAL OF ALREADY COMPUTED RESULTS
+# from simulation A : 20
+SPLIT=1
+idsA=seq((SPLIT-1)*j/6+1,SPLIT*j/6)[1:20]
+# from simulation B : 19
+SPLIT=2
+idsB=seq((SPLIT-1)*j/6+1,SPLIT*j/6)[1:19]
+# from simulation C : 1
+SPLIT=3
+idsC=seq((SPLIT-1)*j/6+1,SPLIT*j/6)[1:1]
+# from simulation D : 42
+SPLIT=4
+idsD=seq((SPLIT-1)*j/6+1,SPLIT*j/6)[1:42]
 
-result=list()
+ids.already=c(idsA,idsB,idsC,idsD)
+rm(idsA,idsB,idsC,idsD,SPLIT)
+
+result<-list()
 
 for(count in 1:k){
-  print(count)
+  if(!(i %in% ids.already))
   rep     <- tunegrid[count,]
   
   control <- rpart.control(minsplit = rep$.minsplit,
@@ -110,13 +121,19 @@ for(count in 1:k){
   lwr_error <- matrix(NA, nrow=5,ncol=30)
   
   for (i in 1:30){
-    ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
+    print(paste(count,",",i))
+    error=1
+    while(error==1){
+      ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
+      if(length(which(d$type[ids.train]=="GCR"))>0) error =0
+    }
+    print("GCRs in the training set")
     ids.train.up=ids.bootstrap.up(ids.train,d$type[ids.train],ratioGT = rep$.ratio)
     test.set=d[-ids.train.up,]
     train.set=d[ids.train.up,]
-    bo <-boosting(type~., data=train.set,mfinal=100,coeflearn = rep$.coeflearn,control = control)
+    bo <-boosting(type~., data=train.set,mfinal=50,coeflearn = rep$.coeflearn,control = control)
     for(s in 1:5){
-      pred=predict(bo,test.set,type = "class",newmfinal = 20*s)
+      pred=predict(bo,test.set,type = "class",newmfinal = 10*s)
       oob_error[s,i] <- length(which((pred$class=="LWR" & test.set$type=="GCR")|(pred$class=="GCR" & test.set$type=="LWR")))/length(pred$class)
       gcr_error[s,i] <- length(which(pred$class=="LWR" & test.set$type=="GCR"))/length(which(test.set$type=="GCR"))
       lwr_error[s,i] <- length(which(pred$class=="GCR" & test.set$type=="LWR"))/length(which(test.set$type=="LWR"))
@@ -129,11 +146,10 @@ for(count in 1:k){
   lwr_list <- rbind(apply(lwr_error,1,mean),
                     apply(lwr_error,1,sd))
   
-  result[[count]] <- rbind(oob_list,gcr_list,lwr_list)
+  result[[1]] <- rbind(oob_list,gcr_list,lwr_list)
+  
+  #======================
+  ## TO CHANGE
+  save(result,file = paste("A/boosting_output2_",count,".RData",sep=""))
+  #======================
 }
-
-save(result, file = paste("boosting_output_",SPLIT,".RData"))
-
-
-
-
