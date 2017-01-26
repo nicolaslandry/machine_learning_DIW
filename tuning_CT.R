bed469c153830cdf8da5248993248b77db7fac3f
nicolaslandry <nicolas.landry@mines-paristech.fr>
2017-01-26 11:09:44 +0100

    Update tuning_CT.R

--- tuning_CT.R
+++ tuning_CT.R
@@ -5,11 +5,14 @@
 
 
 rm(list=ls())
-setwd("H:\\CLASSIFICATION\\")
+setwd("H:\\Machine_Learning\\")
+setwd("/projekte/sseifert/homes/Machine_Learning")
 #=====================================================
 #================= 1 LIBRARIES LOAD ==================
 #=====================================================
 library(rpart)
+library(mlbench)
+library(doParallel)
 
 #=====================================================
 #=============== 3 FUNCTIONS DECLARATION =============
@@ -31,7 +34,7 @@ ids.bootstrap.do<-function(ratioGT,y,size){
   
   idsG2<-sample(x = idsG,size = size/2,replace = FALSE)
   idsL2<-sample(x = idsL,size = size/2,replace = FALSE)
-  #print(idsG2)
+  print(idsG2)
   ids2<-c(idsG2,idsL2)
   #print(length(idsG2)/length(ids2))
   ids2
@@ -41,7 +44,7 @@ ids.bootstrap.do<-function(ratioGT,y,size){
 #=============== 4 LOAD OF THE DATASET ===============
 #=====================================================
 cat("load of the dataset \n")
-alldata=read.csv(file="prisv11.csv",sep=";")
+alldata=read.csv(file="Pris_data_all_with_outages_v05_nl.csv",sep=";")
 ids=which(alldata$type=="PWR"|alldata$type=="BWR")
 levels(alldata$type)=c(levels(alldata$type),"LWR")
 alldata$type[ids]="LWR"
@@ -69,101 +72,74 @@ rm(cnames)
 #================== 5 ANALYSIS =======================
 #=====================================================
 
+#PARALLEL PROCESSING
+max_cores <- detectCores()[1]
+max_cores
+if (max_cores > 25) max_cores <- 7
+cl <- makeCluster(max_cores, homogeneous=T)
+registerDoParallel(cl)
 
 seed=7
 set.seed(seed)
 
 #### WITH FOREACH LOOP
-
 nGCR=length(which(d$type=="GCR"))
-tunegrid.up <- expand.grid(.minsplit=seq(3,40,2),.minbucket=seq(1,20,2),.cp=c(0.00001,0.0001,0.001,0.01,0.1),.tsetsize=seq(100,1100,80),.ratio=seq(0.1,0.8,0.1),.up=TRUE)
-tunegrid.do <- expand.grid(.minsplit=seq(3,40,2),.minbucket=seq(1,20,2),.cp=c(0.00001,0.0001,0.001,0.01,0.1),.tsetsize=seq(10,200,20),.ratio=0.5,.up=FALSE)
+tunegrid.up <- expand.grid(.minsplit=seq(3,40,2),
+                           .minbucket=seq(1,20,2),
+                           .cp=c(0.00001,0.0001,0.001,0.01,0.1),
+                           .tsetsize=seq(100,1100,80),
+                           .ratio=seq(0.1,0.8,0.1),
+                           .up=TRUE)
+tunegrid.do <- expand.grid(.minsplit=seq(3,40,2),
+                           .minbucket=seq(1,20,2),
+                           .cp=c(0.00001,0.0001,0.001,0.01,0.1),
+                           .tsetsize=seq(10,200,20),
+                           .ratio=0.5,
+                           .up=FALSE)
 tunegrid=rbind(tunegrid.up,tunegrid.do)
-
-tunegridbest=data.frame(.minsplit=9,.minbucket=3,.cp=0.01,.tsetsize=190,.ratio=0.5,.up=FALSE)
-
-rep     <- tunegridbest
+rm(tunegrid.do,tunegrid.up)
+k <- nrow(tunegrid)
 
 
-control <- rpart.control(minsplit = rep$.minsplit,
-                         minbucket = rep$.minbucket,
-                         cp = rep$.cp)
+sim <- foreach(count=1:nrow(tunegrid), .packages=c("randomForest","rpart"))%dopar%{
+    rep     <- tunegrid[count,]
 
-oob_error <- matrix(NA, nrow=300,ncol=1)
-gcr_error <- matrix(NA, nrow=300,ncol=1)
-lwr_error <- matrix(NA, nrow=300,ncol=1)
+    
+    control <- rpart.control(minsplit = rep$.minsplit,
+                          minbucket = rep$.minbucket,
+                          cp = rep$.cp)
+    
+    oob_error <- matrix(NA, nrow=30,ncol=1)
+    gcr_error <- matrix(NA, nrow=30,ncol=1)
+    lwr_error <- matrix(NA, nrow=30,ncol=1)
+
+    for (i in 1:30){
+        if(rep$.up){
+          ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
+          ids.train.up=ids.bootstrap.up(ids.train,d$type[ids.train],ratioGT = rep$.ratio)
+          test.set=d[-ids.train.up,]
+          train.set=d[ids.train.up,]
+        }else{
+          ids.train.do=ids.bootstrap.do(d$type,ratioGT = rep$.ratio, size=rep$.tsetsize)
+          test.set=d[-ids.train.do,]
+          train.set=d[ids.train.do,]
+          }
+      
+        ct <- rpart(type~., data=train.set, control = control)
+        pred=predict(ct,test.set,type = "class")
+        
+        oob_error[i] <- length(which((pred=="LWR" & test.set$type=="GCR")|(pred=="GCR" & test.set$type=="LWR")))/length(pred)
+        gcr_error[i] <- length(which(pred=="LWR" & test.set$type=="GCR"))/length(which(test.set$type=="GCR"))
+        lwr_error[i] <- length(which(pred=="GCR" & test.set$type=="LWR"))/length(which(test.set$type=="LWR"))
+    }
 
 
-outF=matrix(NA,1,15)
-colnames(outF)=allVars
-
-for (i in 1:1000){
-  print(i)
-  if(rep$.up){
-    ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
-    ids.train.up=ids.bootstrap.up(ids.train,d$type[ids.train],ratioGT = rep$.ratio)
-    test.set=d[-ids.train.up,]
-    train.set=d[ids.train.up,]
-  }else{
-    ids.train.do=ids.bootstrap.do(d$type,ratioGT = rep$.ratio, size=rep$.tsetsize)
-    test.set=d[-ids.train.do,]
-    train.set=d[ids.train.do,]
-  }
-  
-  ct <- rpart(type~., data=train.set, control = control)
-  
-  fit=ct
-  tmp=rownames(fit$splits)
-  allVars=colnames(attributes(fit$terms)$factors)  
-  rownames(fit$splits)=1:nrow(fit$splits)
-  splits=data.frame(fit$splits)
-  splits$var=tmp
-  splits$type=""
-  frame=as.data.frame(fit$frame)
-  index=0
-  for(i in 1:nrow(frame)){
-    if(frame$var[i] != "<leaf>"){
-      index=index + 1
-      splits$type[index]="primary"
-      if(frame$ncompete[i] > 0){
-        for(j in 1:frame$ncompete[i]){
-          index=index + 1
-          splits$type[index]="competing"}}
-      if(frame$nsurrogate[i] > 0){
-        for(j in 1:frame$nsurrogate[i]){
-          index=index + 1
-          splits$type[index]="surrogate"}}}}
-  splits$var=factor(as.character(splits$var))
-  splits=subset(splits, type != "surrogate")
-  out=aggregate(splits$improve,
-                list(Variable = splits$var),
-                sum, na.rm = TRUE)
-  allVars=colnames(attributes(fit$terms)$factors)
-  if(!all(allVars %in% out$Variable)){
-    missingVars=allVars[!(allVars %in% out$Variable)]
-    zeros=data.frame(x = rep(0, length(missingVars)), Variable = missingVars)
-    out=rbind(out, zeros)}
-  out2=data.frame(Overall = out$x)
-  rownames(out2)=out$Variable
-  outF=rbind(outF,t(out2))
-  
+    oob_list <- rbind(mean(oob_error),sd(oob_error))
+    gcr_list <- rbind(mean(gcr_error),sd(gcr_error))
+    lwr_list <- rbind(mean(lwr_error),sd(lwr_error))
+    
+    rbind(oob_list,gcr_list,lwr_list)
 }
-outF=outF[-1,]
-outFF=rbind(apply(outF,2,mean)-apply(outF,2,sd),2*apply(outF,2,sd))
-barplot(outFF)
-barplot(apply(outF,2,mean))
-plot(density(outF[,5]))
-
-gcr_error2=c(gcr_error,gcr_error2)
-gcr_error=gcr_error2
-rbind(oob_list,gcr_list,lwr_list)
-plot(gcr_error)
-plot(cumsum(gcr_error)/seq_along(gcr_error))
-library(TTR)
-plot(runSD(gcr_error,cumulative = TRUE))
+save(sim, file="simulation_output_ct.RData")
 #SAVING
 #write.table(rf_gridsearch$results,"result_tuning_rf.txt")
-
-
-
-
