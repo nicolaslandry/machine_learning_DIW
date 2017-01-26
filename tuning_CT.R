377182de3e7c869eb3b2bdc6a09d35dc89be4c56
nicolaslandry <nicolas.landry@mines-paristech.fr>
2017-01-26 11:28:19 +0100

    v03
    		- v03 : uses data from v11
    			same tunegrid as v02
    			removal of japanese years
    			the sampling step is slightly different, since we make sure that there is at least 1 GCR in ids.train before upsampling
    			Each combination is saved in a different file. "CT_output_***.RData"

--- tuning_CT.R
+++ tuning_CT.R
@@ -1,21 +1,22 @@
 ## TUNING CLASSIFICATION TREE
 ## NICOLAS LANDRY  created the 15/12/2016
-## LAST MODIFICATION: 15/12/2016
+## LAST MODIFICATION: 12/01/2017
 ##OBJECTIVE: How to improve result from ct?
 
 
 rm(list=ls())
-setwd("H:\\Machine_Learning\\")
-setwd("/projekte/sseifert/homes/Machine_Learning")
+#setwd("H:\\Machine_Learning\\")
+#setwd("/projekte/sseifert/homes/Machine_Learning")
+
+setwd("H:\\CLASSIFICATION\\")
 #=====================================================
 #================= 1 LIBRARIES LOAD ==================
 #=====================================================
 library(rpart)
-library(mlbench)
 library(doParallel)
 
 #=====================================================
-#=============== 3 FUNCTIONS DECLARATION =============
+#=============== 2 FUNCTIONS DECLARATION =============
 #=====================================================
 ids.bootstrap.up<-function(ids,ratioGT,y){
   idsG<-ids[which(y=="GCR")]
@@ -41,19 +42,20 @@ ids.bootstrap.do<-function(ratioGT,y,size){
 }
 
 #=====================================================
-#=============== 4 LOAD OF THE DATASET ===============
+#=============== 3 LOAD OF THE DATASET ===============
 #=====================================================
 cat("load of the dataset \n")
-alldata=read.csv(file="Pris_data_all_with_outages_v05_nl.csv",sep=";")
+alldata=read.csv(file="Pris_data_all_with_outages_v11_nl.csv",sep=";")
 ids=which(alldata$type=="PWR"|alldata$type=="BWR")
 levels(alldata$type)=c(levels(alldata$type),"LWR")
 alldata$type[ids]="LWR"
+ids.japan=which(alldata$country=="JAPAN" & alldata$year>2010)
+alldata=alldata[-ids.japan,]
 
 colnames=c("type","t11","t12","t13","t14","t15","t16","t17","t21","t31","t32","t33","t35","t41","t42","t43")
-#colnames=c("type","t12","t13","t14","t15","t16","t17","t21","t31","t32","t33","t35","t41","t42","t43")
 cnames=which(is.element(colnames(alldata),colnames))
-ids=which(alldata$type=="LWR"|alldata$type=="GCR")
-
+countries=c("FRANCE","GERMANY","JAPAN","SPAIN","SWITZERLAND","UNITED KINGDOM","UNITED STATES OF AMERICA")
+ids=which((alldata$type=="LWR"|alldata$type=="GCR") & alldata$country %in% countries)
 d=alldata[ids,c(cnames)]
 d$type=factor(d$type)
 d=na.omit(d)
@@ -64,12 +66,10 @@ for(i in 1:nrow(d)){
   S[i]=(sum(d[i,2:(ncol(d)-1)]))
 }
 d<-d[-which(S==0),]
-rm(S)
-rm(i)
-rm(ids)
-rm(cnames)
+rm(S,i,ids,cnames,ids.japan,countries)
+
 #=====================================================
-#================== 5 ANALYSIS =======================
+#================== 4 ANALYSIS =======================
 #=====================================================
 
 #PARALLEL PROCESSING
@@ -82,8 +82,6 @@ registerDoParallel(cl)
 seed=7
 set.seed(seed)
 
-#### WITH FOREACH LOOP
-nGCR=length(which(d$type=="GCR"))
 tunegrid.up <- expand.grid(.minsplit=seq(3,40,2),
                            .minbucket=seq(1,20,2),
                            .cp=c(0.00001,0.0001,0.001,0.01,0.1),
@@ -101,45 +99,55 @@ rm(tunegrid.do,tunegrid.up)
 k <- nrow(tunegrid)
 
 
-sim <- foreach(count=1:nrow(tunegrid), .packages=c("randomForest","rpart"))%dopar%{
-    rep     <- tunegrid[count,]
-
-    
-    control <- rpart.control(minsplit = rep$.minsplit,
-                          minbucket = rep$.minbucket,
-                          cp = rep$.cp)
-    
-    oob_error <- matrix(NA, nrow=30,ncol=1)
-    gcr_error <- matrix(NA, nrow=30,ncol=1)
-    lwr_error <- matrix(NA, nrow=30,ncol=1)
-
-    for (i in 1:30){
-        if(rep$.up){
-          ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
-          ids.train.up=ids.bootstrap.up(ids.train,d$type[ids.train],ratioGT = rep$.ratio)
-          test.set=d[-ids.train.up,]
-          train.set=d[ids.train.up,]
-        }else{
-          ids.train.do=ids.bootstrap.do(d$type,ratioGT = rep$.ratio, size=rep$.tsetsize)
-          test.set=d[-ids.train.do,]
-          train.set=d[ids.train.do,]
-          }
-      
-        ct <- rpart(type~., data=train.set, control = control)
-        pred=predict(ct,test.set,type = "class")
-        
-        oob_error[i] <- length(which((pred=="LWR" & test.set$type=="GCR")|(pred=="GCR" & test.set$type=="LWR")))/length(pred)
-        gcr_error[i] <- length(which(pred=="LWR" & test.set$type=="GCR"))/length(which(test.set$type=="GCR"))
-        lwr_error[i] <- length(which(pred=="GCR" & test.set$type=="LWR"))/length(which(test.set$type=="LWR"))
+foreach(count=1:k, .packages=c("randomForest","rpart"))%dopar%{
+  result<-list()
+  rep     <- tunegrid[count,]
+  
+  control <- rpart.control(minsplit = rep$.minsplit,
+                           minbucket = rep$.minbucket,
+                           cp = rep$.cp)
+  
+  oob_error <- matrix(NA, nrow=1,ncol=30)
+  gcr_error <- matrix(NA, nrow=1,ncol=30)
+  lwr_error <- matrix(NA, nrow=1,ncol=30)
+  
+  for (i in 1:30){
+    print(paste(count,",",i))
+    error=1
+    while(error==1){
+      ids.train=sample(1:nrow(d),size = rep$.tsetsize,replace = F)
+      if(length(which(d$type[ids.train]=="GCR"))>0) error =0
     }
-
-
-    oob_list <- rbind(mean(oob_error),sd(oob_error))
-    gcr_list <- rbind(mean(gcr_error),sd(gcr_error))
-    lwr_list <- rbind(mean(lwr_error),sd(lwr_error))
+    print("GCRs in the training set")
+    if(rep$.up){    
+      ids.train.up=ids.bootstrap.up(ids.train,d$type[ids.train],ratioGT = rep$.ratio)
+      test.set=d[-ids.train.up,]
+      train.set=d[ids.train.up,]
+    }else{
+      ids.train.do=ids.bootstrap.do(d$type,ratioGT = rep$.ratio, size=rep$.tsetsize)
+      test.set=d[-ids.train.do,]
+      train.set=d[ids.train.do,]
+    }
     
-    rbind(oob_list,gcr_list,lwr_list)
+    ct <- rpart(type~., data=train.set, control = control)
+    pred=predict(ct,test.set,type = "class")
+    oob_error[1,i] <- length(which((pred=="LWR" & test.set$type=="GCR")|(pred=="GCR" & test.set$type=="LWR")))/length(pred)
+    gcr_error[1,i] <- length(which(pred=="LWR" & test.set$type=="GCR"))/length(which(test.set$type=="GCR"))
+    lwr_error[1,i] <- length(which(pred=="GCR" & test.set$type=="LWR"))/length(which(test.set$type=="LWR"))
+  }
+  
+  oob_list <- rbind(apply(as.matrix(oob_error),1,mean),
+                    apply(as.matrix(oob_error),1,sd))
+  gcr_list <- rbind(apply(gcr_error,1,mean),
+                    apply(gcr_error,1,sd))
+  lwr_list <- rbind(apply(lwr_error,1,mean),
+                    apply(lwr_error,1,sd))
+  
+  result[[1]] <- rbind(oob_list,gcr_list,lwr_list)
+  result[[2]] <- tunegrid[count,]
+  
+  #======================
+  ## TO CHANGE
+  save(result,file = paste("CT_output/CT_output_",count,".RData",sep=""))
+  #======================
 }
-save(sim, file="simulation_output_ct.RData")
-#SAVING
-#write.table(rf_gridsearch$results,"result_tuning_rf.txt")
